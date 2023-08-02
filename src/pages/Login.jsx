fdd118e7f2ec120d1cee06f1d7b32c0a401a79d8
MaharaniJ <maharani.jeyaraman@outlook.com>
2023-08-02 17:27:14 +0530

    deployementurl

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,15 +4,15 @@ import Github from "../img/github.png";
 
 const Login = () => {
   const google = () => {
-    window.open("http://localhost:3000/auth/google", "_self");
+    window.open("https://social-login-backend-7jnn.onrender.com/auth/google", "_self");
   };
 
   const github = () => {
-    window.open("http://localhost:3000/auth/github", "_self");
+    window.open("https://social-login-backend-7jnn.onrender.com/auth/github", "_self");
   };
 
   const facebook = () => {
-    window.open("http://localhost:3000/auth/facebook", "_self");
+    window.open("https://social-login-backend-7jnn.onrender.com/auth/facebook", "_self");
   };
 
   return (
